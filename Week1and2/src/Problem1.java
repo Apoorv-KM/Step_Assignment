7e791d5bb9464f6d42e1ae661183daf88e43be93
Apoorv-KM <apoorv.mi2403@gmail.com>
2026-03-06 15:30:20 +0530

    Problem 1: Social Media Username Availability Checker

--- Week1and2/src/Problem1.java
+++ Week1and2/src/Problem1.java
@@ -1,2 +1,54 @@
+import java.util.*;
+
 public class Problem1 {
+
+    private HashMap<String, Integer> users = new HashMap<>();
+    private HashMap<String, Integer> attempts = new HashMap<>();
+
+    public boolean checkAvailability(String username) {
+        attempts.put(username, attempts.getOrDefault(username, 0) + 1);
+        return !users.containsKey(username);
+    }
+
+    public void registerUser(String username, int userId) {
+        users.put(username, userId);
+    }
+
+    public List<String> suggestAlternatives(String username) {
+        List<String> suggestions = new ArrayList<>();
+
+        suggestions.add(username + "1");
+        suggestions.add(username + "2");
+        suggestions.add(username.replace("_", "."));
+
+        return suggestions;
+    }
+
+    public String getMostAttempted() {
+        String maxUser = "";
+        int max = 0;
+
+        for (String user : attempts.keySet()) {
+            if (attempts.get(user) > max) {
+                max = attempts.get(user);
+                maxUser = user;
+            }
+        }
+
+        return maxUser;
+    }
+
+    public static void main(String[] args) {
+
+        Problem1 system = new Problem1();
+
+        system.registerUser("john_doe", 101);
+
+        System.out.println(system.checkAvailability("john_doe"));
+        System.out.println(system.checkAvailability("jane_smith"));
+
+        System.out.println(system.suggestAlternatives("john_doe"));
+
+        System.out.println("Most attempted: " + system.getMostAttempted());
+    }
 }
